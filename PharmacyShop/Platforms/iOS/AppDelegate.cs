f331653d74791fb500deae82b58ba49ccd94cb4f
NoelStark <noel.k.stark@gmail.com>
2024-11-13 13:14:42 +0100

    Add project files.

---
+++ PharmacyShop/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PharmacyShop
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}

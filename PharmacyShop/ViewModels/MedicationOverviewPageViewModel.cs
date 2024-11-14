1723767eaece3e34adde2488fff22c5f2e000529
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2024-11-14 23:12:55 +0100

    Almost finished product view

--- PharmacyShop/ViewModels/MedicationOverviewPageViewModel.cs
+++ PharmacyShop/ViewModels/MedicationOverviewPageViewModel.cs
@@ -13,7 +13,8 @@ namespace PharmacyShop.ViewModels
     public partial class MedicationOverviewPageViewModel : ObservableObject
     {
         private readonly MedicineConfiguration _medication;
-        List<Medicine> medicationList;
+        
+        private List<Medicine> medicationList;
         public ObservableCollection<Medicine> Medicine { get; set; } = new();
 
         [ObservableProperty]
@@ -23,12 +24,12 @@ namespace PharmacyShop.ViewModels
         public MedicationOverviewPageViewModel(MedicineConfiguration medication)
         {
             _medication = medication;
-            Task await = GetMedicines();
+            Task await = LoadMedicines();
         }
 
 
         [RelayCommand]
-        public async Task GetMedicines()
+        public async Task LoadMedicines()
         {
             medicationList = await _medication.Medicines();
 
@@ -54,7 +55,7 @@ namespace PharmacyShop.ViewModels
         [RelayCommand]
         public void SearchProduct()
         {
-            if (SearchText != "")
+            if (SearchText != string.Empty)
             {
                 var filterBySearchResult = medicationList.Where(a => a.Name.ToLower().Contains(SearchText.ToLower()) || a.Dose.ToLower().Contains(SearchText.ToLower()) || a.Description.ToLower().Contains(SearchText.ToLower())).ToList();
                 Medicine.Clear();
@@ -74,11 +75,20 @@ namespace PharmacyShop.ViewModels
         }
 
         [RelayCommand]
-        public async Task GetMedicine(Medicine medicine)
+        public async Task InspectChosenMedicine(Medicine InspectSelectedMedicine)
         {
-            if(medicine != null)
+            if(InspectSelectedMedicine != null)
             {
-                Console.WriteLine("");
+                
+            }
+        }
+
+        [RelayCommand]
+        public async Task BuyChosenMedicine(Medicine BuySelectedMedicine)
+        {
+            if (BuySelectedMedicine != null)
+            {
+                
             }
         }
     }

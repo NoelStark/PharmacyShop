643a8f0379c2c9c524ed8e69846dcd9b1af5ceda
NoelStark <noel.k.stark@gmail.com>
2024-12-09 14:54:53 +0100

    Bug Fix

--- PharmacyShop/ViewModels/Checkout/ConfirmationViewModels/ConfirmationViewModel.cs
+++ PharmacyShop/ViewModels/Checkout/ConfirmationViewModels/ConfirmationViewModel.cs
@@ -18,8 +18,8 @@ namespace PharmacyShop.ViewModels.Checkout.ConfirmationViewModels
 		public ConfirmationViewModel(PersonService personService)
 		{
 			_personService = personService;
-			Reinitialize();
 			GenerateOrderNumber();
+			Reinitialize();
 		}
 
 		/// <summary>
@@ -67,7 +67,7 @@ namespace PharmacyShop.ViewModels.Checkout.ConfirmationViewModels
 
 		public async Task SupplierEmail()
 		{
-			string productsFromCart = $"Customer Name: {_personService.CurrentPerson.FullName}\nCustomer Email: {_personService.CurrentPerson.Email}\nCustomer Email: {_personService.CurrentPerson.Phone}\nOrdernumber{OrderNumber}\n\nProducts:";
+			string productsFromCart = $"Customer Name: {_personService.CurrentPerson.FullName}\nCustomer Email: {_personService.CurrentPerson.Email}\nCustomer Phone: {_personService.CurrentPerson.Phone}\nOrdernumber{OrderNumber}\n\nProducts:";
 			foreach (var item in ItemsCart)
 			{
                 productsFromCart += $"\nName: {item.Medicine.Name} {item.Medicine.Dose} {item.Medicine.Description}\nArticlenumber: {item.Medicine.ArticleNumber}, Quantity: {item.Quantity}\n";
